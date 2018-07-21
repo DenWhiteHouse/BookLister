f373b14a7ecdf9f0a50f7f6f88a98ef998ceff08
DennyWhiteHouse <casabiancadenny@gmail.com>
2018-07-21 08:51:58 +0200

    Added BookUtil for getting retrofit object

---
+++ app/src/main/java/com/example/denny/booklister/Book/BookUtil.java
@@ -0,0 +1,47 @@
+package com.example.denny.booklister.Book;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.denny.booklister.BookData.Book;
+import com.example.denny.booklister.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BookUtil {
+    Book mBook;
+    Context mContext;
+
+    public BookUtil(Context context){
+        mContext = context;
+        mBook = new Book();
+    }
+
+    public Book getBookJSON(String bookTitle) {
+        //Getting the JSON Object with Retrofit
+        BooksInterface booksInterface = RetrofitClient.getRetrofitInstance();
+        Call<Book> book = booksInterface.getBooks(bookTitle);
+
+        book.enqueue(new Callback<Book>() {
+            @Override
+            public void onResponse(Call<Book> call, Response<Book> response) {
+                if (response.isSuccessful() && response.body().getItems() != null) {
+                    mBook = response.body();
+                    }
+            }
+
+            @Override
+            public void onFailure(Call<Book> call, Throwable t) {
+                Toast.makeText(mContext, R.string.error_message_retrofit, Toast.LENGTH_SHORT).show();
+                Log.v("Retrofit has failed ", t.getMessage());
+            }
+        });
+
+        return mBook;
+    }
+
+}
